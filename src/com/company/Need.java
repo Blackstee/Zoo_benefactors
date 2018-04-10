57ff68bcb4cc88cc0d233f02f4619454e915c269
Blackstee <ya.nastuha24@gmail.com>
2018-04-10 22:05:50 +0300

    added need

--- src/com/company/Need.java
+++ src/com/company/Need.java
@@ -4,4 +4,55 @@ package com.company;
  * Created by blackstee on 10.04.18.
  */
 public class Need {
+    public String name;
+    public String type;
+    public int amount;
+    public int price;
+
+
+    public Need(String name, String type, int amount, int price) {
+        this.name = name;
+        this.type = type;
+        this.amount = amount;
+        this.price = price;
+
+    }
+
+    public String get_name(){
+        return name;
+    }
+
+    public String get_type(){
+        return type;
+    }
+
+    public int get_amount(){
+        return amount;
+    }
+
+    public int get_price(){
+        return price;
+    }
+
+
+    public String get_all_info(){
+        return
+                "\n\t Need:\n\t\t Name: " + name + "\n\t\t Type: " + type + "\n\t\t Amount: " + amount + "\n\t\t Price: " + price + "\n";
+    }
+
+    public void add_amount(int value){
+        amount += value;
+        }
+
+    public String reduce_amount(int value){
+        if (amount - value < 0){
+            return "\n Not enough value of need...\n Current: " + amount + "\n";
+        }
+        else
+        {
+            amount -= value;
+            return "\n Amount was successfully reduced! \n Current: " + amount + "\n";
+        }
+
+    }
 }
